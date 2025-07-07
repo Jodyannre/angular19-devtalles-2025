644cb9d1c72f8646f1b2fbe06a81a6700159d69f
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2025-07-06 23:44:49 -0600

    mod 6 - 72

---
+++ 003-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface SideMenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [ RouterLink, RouterLinkActive ],
+  templateUrl: './side-menu-options.component.html',
+})
+export class SideMenuOptionsComponent {
+
+  menuOptions: SideMenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      subLabel: 'Gifs Populares'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      route: '/dashboard/search',
+      subLabel: 'Buscar Gifs'
+    }
+  ]
+}
