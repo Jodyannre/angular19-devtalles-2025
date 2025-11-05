0107f4fc910f0670d5907c95ec4e2f861db0a083
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2025-11-04 23:17:52 -0600

    mod 9 - 109

---
+++ 004-country-app/src/app/country/country.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { CountryLayout } from './layouts/CountryLayout/CountryLayout';
+import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
+
+
+export const countryRoutes: Routes = [
+
+  {
+    path: '',
+    component: CountryLayout,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPage
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
+  }
+
+];
+
+
+export default countryRoutes;
